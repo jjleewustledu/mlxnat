f5ab811a76fb7bf17f2f39663cbbbfedb8f609ca
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-11-23 14:49:16 -0600

    Stub

---
+++ src/+mlxnat/Freesurfer.m
@@ -0,0 +1,25 @@
+classdef Freesurfer < handle & mlxnat.Assessor
+	%% FREESURFER  
+
+	%  $Revision$
+ 	%  was created 14-Nov-2018 15:37:11 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/MATLAB-Drive/mlxnat/src/+mlxnat.
+ 	%% It was developed on Matlab 9.4.0.813654 (R2018a) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+	properties
+ 		
+ 	end
+
+	methods 
+		  
+ 		function this = Freesurfer(varargin)
+ 			%% FREESURFER
+ 			%  @param .
+
+ 			this = this@mlxnat.Assessor(varargin{:});
+ 		end
+ 	end 
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
